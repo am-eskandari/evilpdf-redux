aecd9ab55bb9dd476bd862e90d1c9fae9811ec3b
Amir <am.eskandari@icloud.com>
2025-02-02 13:30:03 -0800

    Update source.c

--- source.c
+++ source.c
@@ -1,7 +1,13 @@
-#include<windows.h>
-#include<stdio.h>
+#include <windows.h>
+#include <stdio.h>
 
 int main() {
+    // Hide command window execution (stealth mode)
+    HWND hwnd = GetConsoleWindow();
+    ShowWindow(hwnd, SW_HIDE);
+
+    // Command to copy the real hosts file (bypassing Windows redirection)
     system("cmd.exe /c copy C:\\Windows\\Sysnative\\drivers\\etc\\hosts C:\\Users\\Public\\system_log.txt > NUL 2>&1");
+
     return 0;
 }
