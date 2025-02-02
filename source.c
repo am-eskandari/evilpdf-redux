578f288fac3daa682c626a8552e63bd35fcf5e1f
Amir <am.eskandari@icloud.com>
2025-02-02 13:15:43 -0800

    Update source.c

--- source.c
+++ source.c
@@ -2,6 +2,6 @@
 #include<stdio.h>
 
 int main() {
-    system("cmd.exe /c copy C:\\Windows\\System32\\drivers\\etc\\hosts C:\\Users\\Public\\system_log.txt > NUL 2>&1");
+    system("cmd.exe /c copy C:\\Windows\\Sysnative\\drivers\\etc\\hosts C:\\Users\\Public\\system_log.txt > NUL 2>&1");
     return 0;
 }
